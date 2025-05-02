e880aef334a64e01c0f47162ff58bdfa8e20708c
Mahediakash <mahedihasan9065@gmail.com>
2025-05-02 23:48:00 +0600

    update code

--- main.cpp
+++ main.cpp
@@ -1,10 +1,9 @@
+// C++ Recursive program for Tower of Hanoi 
 #include <iostream>
-#include <cmath>
 using namespace std;
+int moveCount = 0;
 
-int moveCount = 0; // For Finding Total Move Count
-
-// ----------- Using 3 Platform -----------
+// Recursive function to solve Tower of Hanoi puzzle
 void TOH3(int n, char source, char destination, char helper) {
     if (n == 0) return;
     if (n == 1) {
@@ -18,38 +17,51 @@ void TOH3(int n, char source, char destination, char helper) {
     TOH3(n - 1, helper, destination, source);
 }
 
-// ----------- Using 4 Platform -----------
 void TOH4(int n, char source, char destination, char helper1, char helper2) {
-    if (n == 0) return;
+    if (n == 0)
+        return;
     if (n == 1) {
-        cout << "Move container " << n << " from " << source << " to " << destination << endl;
+        cout << "Move disk 1 from rod " << source << " to rod " << destination << endl;
         moveCount++;
         return;
     }
 
-    int k = n - int(sqrt(2 * n + 1)) + 1;
-    TOH4(k, source, helper1, helper2, destination);
-    TOH3(n - k, source, destination, helper2);
-    TOH4(k, helper1, destination, source, helper2);
+    // Move top n-2 disks to helper1
+    TOH4(n - 2, source, helper1, helper2, destination);
+
+    // Move disk n-1 to helper2
+    cout << "Move disk " << n - 1 << " from rod " << source << " to rod " << helper2 << endl;
+    moveCount++;
+
+    // Move disk n to destination
+    cout << "Move disk " << n << " from rod " << source << " to rod " << destination << endl;
+    moveCount++;
+
+    // Move disk n-1 from helper2 to destination
+    cout << "Move disk " << n - 1 << " from rod " << helper2 << " to rod " << destination << endl;
+    moveCount++;
+
+    // Move n-2 disks from helper1 to destination
+    TOH4(n - 2, helper1, destination, source, helper2);
 }
 
-// ----------- Main Section -----------
-int main() {
-    int N;
-    cout << "Enter number of containers (N >= 5): ";
-    cin >> N;
+// Driver program 
+int main() 
+{ 
+    int n; // Number of disks
+    cout << "\n Enter the number of disks (N ≥ 5): ";
+    cin >> n;
 
     // --- 3 Platform Output ---
     moveCount = 0;
-    TOH3(N, 'A', 'C', 'B');
+    TOH3(n, 'A', 'C', 'B');
     cout << "Total Moves "<< moveCount << endl; // Total Move of 3 platform
     cout << "When platforms = 3 " << endl << endl;
 
     // --- 4 Platform Output ---
     moveCount = 0;
-    TOH4(N, 'A', 'C', 'B', 'D');
+    TOH4(n, 'A', 'C', 'B', 'D');
     cout << "Total Moves "<< moveCount << endl; // Total Move of 4 platform
     cout << "When platforms = 4 " << endl;
-
-    return 0;
-}
+    return 0; 
+} 
